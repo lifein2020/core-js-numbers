827c512276b9274f979e53befb81269fb8b5d4ab
Svetlana Zh <lady_zhivtsova@gmail.com>
2025-10-21 01:57:03 +0300

    feat: implement getLastDigit parseNumberFromString getParallelepipedDiagona

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -128,8 +128,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return Number.isFinite(value) ? value % 10 : 'Wrong value';
 }
 
 /**
@@ -143,8 +143,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return value.toString();
 }
 
 /**
@@ -160,8 +160,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  if (a < 0 || b < 0 || c < 0) return 'Wrong param(s)';
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
